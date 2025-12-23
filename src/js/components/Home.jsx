faa589a5a66c80563667f7c0a2bc31bf5f6a69a4
Lgsus23 <jesusbernardops23@gmail.com>
2025-12-23 13:44:18 +0000

    primera revision con bonus

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,46 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+
+
+
+const Home = () => {
+	const [color, setColor] = useState("");
+
+	//--------------------------- FUNCIÓN PARA QUE SE ILUMINE EL CÍRCULO AL QUE SE CLICA -----------------------------
+	const handleClick = (e)=>{
+		setColor(e.target.id)
+		
+		return
+	}
+
+	//------------------------------- FUNCIÓN PARA EL BOTÓN QUE ALTERNA LAS LUCES ENCENDIDAS -----------------------------
+	const alternarClick = (e) =>{
+		if (color === "") setColor("redLight")
+		if (color === "redLight") setColor("yellowLight")
+		if (color === "yellowLight") setColor("greenLight")
+		if (color === "greenLight") setColor("redLight")
+
+			return
+	}
+
+	return (
+		<div className="text-center container">
+			<div className="rectangle">
+				<div id="redLight" className={color === "redLight" ? color : ""} onClick={handleClick}></div>
+				<div id="yellowLight" className={color === "yellowLight" ? color : ""} onClick={handleClick}></div>
+				<div id="greenLight" className={color === "greenLight" ? color : ""} onClick={handleClick}></div>
+			</div>
+			<div className="botones d-flex flex-column col-6 col-md-3 text-center mx-auto">
+				<button className="button" role="button" onClick={handleClick}>Apagar</button>
+				<button className="button mt-3" role="button" onClick={alternarClick}>Alternar</button>
+			</div>
 		</div>
 	);
 };
